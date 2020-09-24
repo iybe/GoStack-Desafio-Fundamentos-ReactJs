c87def3e1143952c3820e0b966837f9a1d0f5c6a
iybe <iesleybezerra2@gmail.com>
2020-09-24 12:22:04 -0300

    subindo

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,9 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  const s = new Intl.NumberFormat('de-DE', {
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2,
+  }).format(value);
+  return `R$ ${s}`;
+};
 
 export default formatValue;
